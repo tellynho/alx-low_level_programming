ef66664ad94ef82c4eab55e9f50602fa8d5d1239
root <elves.teles46@gmail.com>
2022-12-09 13:44:49 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+/**
+ *
+ * *main - Program to print alphabet letter followed by new line
+ *
+ * *
+ *
+ * *Return: return 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		char c = 'a';
+
+			while (c <= 'z')
+
+					{
+
+								putchar(c);
+
+										c++;
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+}
+
+
