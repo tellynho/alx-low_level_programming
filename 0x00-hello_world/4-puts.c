fb1ecb91e318726c0c25c1db817528ff2bfaffb7
root <elves.teles46@gmail.com>
2022-12-08 17:39:47 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,7 @@
 
 int main(void)
 {
-       puts("\"Programing is like building a multilingual puzzle");      
+       puts("\"Programing is like building a multilingual puzzle");
+      
        return (0);
 }       
