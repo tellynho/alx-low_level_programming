6c01ec3b670a91ec6342aac6687c99fb28e5fc15
root <elves.teles46@gmail.com>
2022-12-09 13:59:14 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -23,3 +23,23 @@ int main(void)
 			for (letter = 'a'; letter <= 'z'; letter++)
 
 					{
+
+								if (letter == 'q' || letter == 'e')
+
+											{
+
+															continue;
+
+																	}
+
+										putchar(letter);
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+}
+
+
