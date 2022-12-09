ffc4035cbef2bbb8f037f0447130ee78cb36cc7e
root <elves.teles46@gmail.com>
2022-12-08 17:58:50 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper gramar,
+ * but the outcome is a piece of art,
+ * Return: o if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+       printf("with proper gramar,but the outcome is a piece of art,\n");
+       return (0);
+}       
