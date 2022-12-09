7f379706efc891b718216ac30ea4b2fb6decabf2
root <elves.teles46@gmail.com>
2022-12-09 13:54:22 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+/**
+ *
+ * *main - Program to print alphabet letters except q and e followed by new line
+ *
+ * *
+ *
+ * *Return: return 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		int letter;
+
+
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+
+					{
